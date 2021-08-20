0810d30564670afdf6457934a90b27dd9a55b7a1
rarodrigues9 <86211399+rarodrigues9@users.noreply.github.com>
2021-08-20 13:22:26 -0400

    Update install.sh
    Install docker, docker-compose and kubectl

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Install Docker
 sudo curl https://releases.rancher.com/install-docker/20.10.sh | sh
 sudo usermod -aG docker vagrant
-sudo apt install git -y
+
+# Install Docker-Compose
+sudo apt install git gnupg2 -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# Install Kubeclt
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt update && sudo apt install kubectl -y
